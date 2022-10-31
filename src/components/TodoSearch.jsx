37a61652fb992c08af0877ef9b2ebe87286245d3
LuisArmandoOlmos <luisarmandoolmosolmos@gmail.com>
2022-10-31 15:13:51 -0400

    Add useState searchValue

--- src/components/TodoSearch.jsx
+++ src/components/TodoSearch.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
+
 export const TodoSearch = () => {
-  const onChangeSearchValue = ({ target }) => console.log(target.value);
+  const [searchValue, setSearchValue] = useState("");
+  const onChangeSearchValue = ({ target }) => {
+    console.log(target.value);
+    setSearchValue(target.value);
+  };
 
   return (
-    <input
-      className="bg-main-white border-2 border-main-black border-solid focus:outline-main-cian font-montserrat font-normal h-16 mx-6 placeholder-main-gray rounded-md shadow-2xl
-      text-2xl text-center text-main-dark w-[calc(100%-62px)]"
-      onChange={onChangeSearchValue}
-      placeholder="Cebolla"
-    />
+    <>
+      <input
+        className="bg-main-white border-2 border-main-black border-solid focus:outline-main-cian font-montserrat font-normal h-16 mx-6 placeholder-main-gray rounded-md shadow-2xl
+        text-2xl text-center text-main-dark w-[calc(100%-62px)]"
+        onChange={onChangeSearchValue}
+        placeholder="Cebolla"
+      />
+    </>
   );
 };
